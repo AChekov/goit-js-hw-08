72fa0ed33a9bcbb45a7618fd60ebcb0c614f191f
AChekov <4ekoff@gmail.com>
2021-12-07 23:29:49 +0200

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,38 @@
+import throttle from 'lodash.throttle';
 
+const form = document.querySelector('.feedback-form');
+const STORAGE_KEY = 'feedback-form-state';
+
+form.addEventListener('submit', onFormSubmit);
+form.addEventListener('input', throttle(onFormInput, 500));
+
+function onFormSubmit(evt) {
+  evt.preventDefault();
+  console.log({
+    email: form.email.value,
+    message: form.message.value,
+  });
+  evt.currentTarget.reset();
+  localStorage.removeItem(STORAGE_KEY);
+}
+
+function onFormInput() {
+  const formData = {
+    email: form.email.value,
+    message: form.message.value,
+  };
+
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+}
+
+function populateInput() {
+  const savedForm = localStorage.getItem(STORAGE_KEY);
+  const parsedForm = JSON.parse(savedForm);
+
+  if (savedForm) {
+    form.email.value = parsedForm.email;
+    form.message.value = parsedForm.message;
+  }
+}
+
+populateInput();
